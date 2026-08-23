889a0c9fdc9aa37d21fe2b1999b9c0fbdfa06294
Shi Lei <shi_lei@massclouds.com>
2026-08-23 17:29:08 +0800

    Refactor RISC-V entry as naked function

--- impl/phases/arch_head.rs
+++ impl/phases/arch_head.rs
@@ -1,17 +1,8 @@
 //! Architecture-specific machine entry.
 
-use core::arch::global_asm;
-
-global_asm!(
-    r#"
-    .section .head.text.entry, "ax"
-    .align 2
-    .globl _start
-    .type _start, @function
-_start:
-1:
-    wfi
-    j 1b
-    .size _start, . - _start
-"#,
-);
+#[unsafe(naked)]
+#[unsafe(export_name = "_start")]
+#[unsafe(link_section = ".head.text.entry")]
+pub unsafe extern "C" fn boot_entry(_hart_id: usize, _dtb: usize) -> ! {
+    core::arch::naked_asm!("1:", "wfi", "j 1b",);
+}
